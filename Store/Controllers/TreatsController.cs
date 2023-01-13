c266c1030465904f3d22cd264bba644c831e5b0d
bodwood <bodiewood@hotmail.com>
2023-01-13 14:26:18 -0800

    add method to join flavors with treats

---
+++ Store/Controllers/TreatsController.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Store.Models;
+
+namespace Store.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly StoreContext _db;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, StoreContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      ViewBag.Flavors = _db.Flavors.ToList();
+      Treat thisTreat = _db.Treats
+                                  .Include(treat => treat.JoinEntities)
+                                  .ThenInclude(join => join.Flavor)
+                                  .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    public ActionResult Edit()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult EditTreat()
+    {
+      return RedirectToAction("Details");
+    }
+
+    public ActionResult Delete()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult DeleteTreat()
+    {
+      return RedirectToAction("Index");
+    }
+  }
+}
